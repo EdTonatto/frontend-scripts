f0ae936654286bb6135f21cf1f35a1d535dfbcec
Eduardo Tonatto <edtonatto@gmail.com>
2020-02-13 20:58:04 -0300

    :sparkles: feat(api): implement connection with API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import './App.css'
+import api from './services/api'
 
 function App() {
+  const [connection, setConnection] = useState()
+
+  useEffect(() =>{
+    async function loadConnection(){
+      const res = await api.get('/');
+      
+      setConnection(res.data);
+    }
+
+    loadConnection();
+  }, [])
+
   return (
-    <h1>Hello World</h1>
+    <h1>{JSON.stringify(connection)}</h1>
   );
 }
 
