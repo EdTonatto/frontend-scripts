d4ac5f9f70a89045cebc33ff34336017b22d7542
Eduardo <edtonatto@gmail.com>
2020-02-17 16:23:49 -0300

    :construction: feat(register): codemirror now is full div size

--- src/components/NewScript/index.js
+++ src/components/NewScript/index.js
@@ -1,20 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
 import CodeMirror from 'react-codemirror'
 
 import './style.css'
 
 function NewScript(){
     require('codemirror/mode/sql/sql')
+    const [code, setCode] = useState("")
+
     const options = {
         lineNumbers: true,
         mode: "text/x-mssql"
     };
 
+    function updateCode(newCode){
+        setCode(newCode)
+    }
+
     return(
         <>
         <span id="title">Cadastro de Scripts</span>
         <div className="code-editor">
-            <CodeMirror options={options}/>
+            <CodeMirror options={options} onChange={updateCode}/>
         </div>
         </>
     )
