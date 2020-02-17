1d234dc8390db914eb2a48abd9e99f5f436093c5
Eduardo <edtonatto@gmail.com>
2020-02-17 13:53:06 -0300

    :sparkles: feat(delete): add a button to delete scripts
    Add a button with trash icon on the list, by clicking in it the selected script will be deleted

--- src/components/ScriptBox/ScriptRow/index.js
+++ src/components/ScriptBox/ScriptRow/index.js
@@ -1,11 +1,30 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+
+import api from '../../../services/api'
+
+import './style.css'
 
 function ScriptRow({script}){
+    async function handleDeleteClick(e){
+        e.preventDefault();
+        alert(script._id)
+
+        const res = await api.delete(`/scripts/${script._id}`)
+        
+        alert(res.data.message)
+    }
+
     return(
         <tr>
             <td>{script.name}</td>
             <td>{script.author}</td>
-            <td>X</td>
+            <td className="option">
+                <button onClick={handleDeleteClick}>
+                    <FontAwesomeIcon icon={faTrashAlt} />
+                </button>
+            </td>
         </tr>  
     )
 }
