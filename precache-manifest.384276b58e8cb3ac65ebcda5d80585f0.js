28d67b2320295342cdeab02c7ba2f67fe25bdb38
Eduardo Tonatto <edtonatto@gmail.com>
2020-02-18 23:56:07 -0300

    Updates

---
+++ precache-manifest.384276b58e8cb3ac65ebcda5d80585f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb60cdaef1d5277c77689ea11cb97e9c",
+    "url": "/frontend-scripts/index.html"
+  },
+  {
+    "revision": "6eddaeb10923889f50ec",
+    "url": "/frontend-scripts/static/css/2.56a1fa8e.chunk.css"
+  },
+  {
+    "revision": "b51d476c802ef03ef172",
+    "url": "/frontend-scripts/static/css/main.ef2eeb68.chunk.css"
+  },
+  {
+    "revision": "6eddaeb10923889f50ec",
+    "url": "/frontend-scripts/static/js/2.09d79c0c.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/frontend-scripts/static/js/2.09d79c0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b51d476c802ef03ef172",
+    "url": "/frontend-scripts/static/js/main.a02b95fb.chunk.js"
+  },
+  {
+    "revision": "ee0224f9754479644905",
+    "url": "/frontend-scripts/static/js/runtime-main.48b6d68c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/frontend-scripts/precache-manifest.d4dd47a100b23543d8cea827564473d3.js"
+  "/frontend-scripts/precache-manifest.384276b58e8cb3ac65ebcda5d80585f0.js"
 );
 
 self.addEventListener('message', (event) => {
